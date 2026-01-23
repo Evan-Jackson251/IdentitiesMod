4d4ff3cd47269a36beb8b3b4d35234aab1401280
Sam <sam.h.kurtz@gmail.com>
2026-01-23 00:16:38 -0500

    Time Stop Cooldown

--- src/main/java/com/schnozz/identitiesmod/events/time_lord/ClientTimeLordEvents.java
+++ src/main/java/com/schnozz/identitiesmod/events/time_lord/ClientTimeLordEvents.java
@@ -2,10 +2,15 @@ package com.schnozz.identitiesmod.events.time_lord;
 
 import com.schnozz.identitiesmod.IdentitiesMod;
 import com.schnozz.identitiesmod.attachments.ModDataAttachments;
+import com.schnozz.identitiesmod.cooldown.Cooldown;
+import com.schnozz.identitiesmod.cooldown.CooldownAttachment;
 import com.schnozz.identitiesmod.damage_sources.ModDamageTypes;
 import com.schnozz.identitiesmod.networking.payloads.EntityDamagePayload;
+import com.schnozz.identitiesmod.networking.payloads.sync_payloads.CooldownSyncPayload;
 import com.schnozz.identitiesmod.networking.payloads.sync_payloads.TimeStopSyncPayload;
+import com.schnozz.identitiesmod.screen.icon.CooldownIcon;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
@@ -16,6 +21,7 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 import java.util.Iterator;
@@ -28,6 +34,8 @@ import static com.schnozz.identitiesmod.keymapping.ModMappings.TIME_STOP_MAPPING
 public class ClientTimeLordEvents {
     private static final int STOP_DURATION = 120;
     private static int timeCounter = 0;
+    private static final CooldownIcon TIME_STOP_COOLDOWN_ICON = new CooldownIcon(10, 10, 16, ResourceLocation.fromNamespaceAndPath(IdentitiesMod.MODID, "textures/gui/clock_icon.png"));
+    private static final int TIME_STOP_CD = 1300;
     @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Post event) {
         LocalPlayer timePlayer = Minecraft.getInstance().player;
@@ -37,10 +45,18 @@ public class ClientTimeLordEvents {
 
         String power = timePlayer.getData(ModDataAttachments.POWER_TYPE);
         if (power.equals("Time Lord")) {
-            if(TIME_STOP_MAPPING.get().consumeClick())
+            if(TIME_STOP_MAPPING.get().consumeClick() && !timePlayer.getData(ModDataAttachments.COOLDOWN).isOnCooldown(ResourceLocation.fromNamespaceAndPath(IdentitiesMod.MODID, "time_stop_cd"),0))
             {
                 timePlayer.setData(ModDataAttachments.TIME_STOP_STATE,1);
                 PacketDistributor.sendToServer(new TimeStopSyncPayload(1));
+
+                long currentTime = Minecraft.getInstance().level.getGameTime();
+                CooldownAttachment atachment = new CooldownAttachment();
+                atachment.getAllCooldowns().putAll(timePlayer.getData(ModDataAttachments.COOLDOWN).getAllCooldowns());
+                atachment.setCooldown(ResourceLocation.fromNamespaceAndPath("identitiesmod", "time_stop_cd"), currentTime, TIME_STOP_CD);
+                timePlayer.setData(ModDataAttachments.COOLDOWN, atachment);
+                PacketDistributor.sendToServer(new CooldownSyncPayload(new Cooldown(currentTime, TIME_STOP_CD), ResourceLocation.fromNamespaceAndPath("identitiesmod", "time_stop_cd"), false));
+                TIME_STOP_COOLDOWN_ICON.setCooldown(new Cooldown(currentTime, TIME_STOP_CD));
             }
 
             if(timePlayer.getData(ModDataAttachments.TIME_STOP_STATE) == 1)
@@ -66,5 +82,16 @@ public class ClientTimeLordEvents {
                 }
             }
         }
+    }
+    @SubscribeEvent
+    public static void onRenderOverlay(RenderGuiEvent.Post event) {
+        if(!Minecraft.getInstance().player.getData(ModDataAttachments.POWER_TYPE).equals("Time Lord"))
+        {
+            return;
+        }
+
+        long gameTime = Minecraft.getInstance().level.getGameTime();
+        GuiGraphics graphics = event.getGuiGraphics();
+        TIME_STOP_COOLDOWN_ICON.render(graphics, gameTime);
     }
 }
